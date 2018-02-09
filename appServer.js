0d1fa4b883c858bc6077af5e553f3c66de1c73a2
asionius <814181242@qq.com>
2018-02-09 12:06:05 +0800

    auth user with jws

--- appServer.js
+++ appServer.js
@@ -2,9 +2,102 @@ const uuid = require('uuid');
 const http = require('http');
 const hash = require('hash');
 const fs = require('fs');
+const jws = require('fib-jws');
+const rados = require('./rados');
 const config = require('./config');
 
+let privPem = fs.readFile('./pem/rsa_private_key.pem');
+let pubPem = fs.readFile('./pem/rsa_public_key.pem');
 let sessionStorage = {};
+let response = (v, data) => {
+    v.response.write(typeof (data) === 'object' ? JSON.stringify(data) : data);
+}
+config.log.forEach((log) => {
+    console.add(log);
+})
+let svr = new http.Server(config.server.port, [(v) => {
+    let jws = v.cookies['jws'];
+    v.jws = jws;
+}, {
+    'signup': (v) => {
+        let username = v.form.username;
+        let phone = v.form.phone;
+        let password = v.form.password;
+        rados.setUser(username, {
+            password: password,
+            phone: phone
+        })
+        response(v, {
+            code: 0,
+            msg: "success"
+        })
+    },
+    'login': (v) => {
+        let username = v.form.username;
+        let password = v.form.password;
+        let userInfo = rados.getUser(username);
+        if (userInfo.password === password) {
+            let signature = jws.sign({
+                alg: "RS512"
+            }, {
+                username: username
+            }, privPem);
+            v.response.addHeader('set-Cookie', "jws=" + signature + "; Expires=" + new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toGMTString() + "; path=/; domain=" + config.server.domain);
+            response(v, {
+                code: 0,
+                msg: "success"
+            })
 
-let svr = new http.Server(8088, [(v) => {
-}, {}]);
+            return;
+        }
+        response(v, {
+            code: 1000,
+            msg: "password or username wrong"
+        })
+    },
+    '_search': (v) => {
+        if (!v.jws || !jws.verify(v.jws, pubPem)) {
+            response(v, {
+                code: 3000,
+                msg: "need login"
+            })
+            return;
+        }
+        let form = v.form.toJSON();
+        let servers = form.servers;
+        let timeRange = form.TimeRange;
+        let content = form.content;
+        if (!servers || servers.length === 0) {
+            let errMsg = "search: servers is null"
+            console.error(errMsg)
+            response(v, {
+                code: 2000,
+                msg: errMsg
+            });
+        }
+        if (!timeRange || timeRange.length === 0) {
+            let errMsg = "search: timeRange is null"
+            console.error(errMsg)
+            response(v, {
+                code: 2000,
+                msg: errMsg
+            });
+        }
+        if (!content) {
+            let errMsg = "search: content is null"
+            console.error(errMsg)
+            response(v, {
+                code: 2000,
+                msg: errMsg
+            });
+        }
+        let ret = rados.search(servers, timeRange, content);
+        response(v, {
+            code: 0,
+            data: ret.join('\n')
+        });
+    },
+    '*': (v) => {
+        return http.fileHandler('./public');
+    }
+}]);
--- rados.js
+++ rados.js
@@ -1,53 +1,66 @@
 const config = require('./config').rados;
 const rados = require('rados');
 const hs = require('hyperscan');
+const fs = require('fs');
 const io = require('io');
 const pool = require('fib-pool');
 const zlib = require('zlib');
+require('./dateFormat');
 
 let eolReg = hs.compile("\n", "L");
 
 let cluster = new rados.Rados(config.clustername, config.username, config.conf);
 let radosPool = pool(() => {
     cluster.connect();
-    return cluster.createIoCtx('logs')
+    return cluster.createIoCtx(config.poolname)
+})
+let authPool = pool(() => {
+    return cluster.createIoCtx(config.authpool)
 })
 
+function getUser(username) {
+    let stream = authPool((conn) => {
+        return conn.open(username);
+    })
+    let buf = stream.read();
+    stream.close();
+    if (buf) return JSON.parse(buf.toString());
+    else return null;
+}
+
+function setUser(username, userInfo) {
+    let stream = authPool((conn) => {
+        return conn.open(username);
+    })
+    stream.write(JSON.stringify(userInfo));
+    stream.close();
+}
+
 function splitTime(timeRange) {
-    return ['2017-08-28/04:28']
+    if (timeRange.length === 1)
+    {
+        return [new Date(timeRange[0]).format('yyyy-MM-dd/hh:mm')];
+    }
+    let t, r = [];
+    let s = new Date(timeRange[0]).getTime();
+    let e = new Date(timeRange[1]).getTime();
+    while (t = (s + 1000 * 60) <= e) {
+        r.push(new Date(t).format('yyyy-MM-dd/hh:mm'))
+    }
 }
 
 function search(servers, timeRange, content) {
-    if (!servers || servers.length === 0) {
-        let errMsg = "search: servers is null"
-        console.error(errMsg)
-        return {
-            error: errMsg
-        }
-    }
-    if (!timeRange || timeRange.length === 0) {
-        let errMsg = "search: timeRange is null"
-        console.error(errMsg)
-        return {
-            error: errMsg
-        }
-    }
-    if (!content) {
-        let errMsg = "search: content is null"
-        console.error(errMsg)
-        return {
-            error: errMsg
-        }
-    }
     let ret = [];
     let hsReg = hs.compile(content, "L");
     timeRange = splitTime(timeRange);
     servers.forEach(svr => {
         timeRange.forEach((time) => {
             let stream = radosPool((conn) => {
-                conn.open(svr + '/' + time)
+                let key = svr + '/' + time;
+                return conn.open(key);
             });
-            let buf, ms, uzs, left, eolRes, pos = 0;
+            let buf, ms, uzs, left, eolRes, pos = 0,
+                lline = 0;
             ms = new io.MemoryStream();
             uzs = zlib.createGunzip(ms);
             while (stream.copyTo(uzs, 1000000) !== 0) {
@@ -68,30 +81,37 @@ function search(servers, timeRange, content) {
                 buf = buf.slice(0, lid);
                 let hsRes = hsReg.scan(buf);
                 if (!hsRes) continue;
-                let lline = 0;
                 hsRes[content].forEach((pair) => {
-                    for(var i = lline; i < eolRes['\n'].length; i++) {
+                    for (var i = lline; i < eolRes['\n'].length; i++) {
                         let eolPair = eolRes['\n'][i];
-                        
-                        if(pair[1] < eolPair[1]) {
-                            lline  = i;
-                            if(lline === 0) 
-                                ret.push(buf.slice(0, eolPair[0]).toString())
-                            else
-                            {
+
+                        if (pair[1] < eolPair[1]) {
+                            lline = i;
+                            if (lline === 0) {
+                                let rs = buf.slice(0, eolPair[0]).toString()
+                                ret.push(rs)
+                            } else {
                                 let lastPair = eolRes['\n'][i - 1];
-                                ret.push(buf.slice(lastPair[1], eolPair[0]).toString());
+                                let rs = buf.slice(lastPair[1], eolPair[0]).toString()
+                                ret.push(rs);
                             }
                             break;
                         }
                     }
                 })
             }
-            if(hsReg.scan(left))
+            if (hsReg.scan(left)) {
                 ret.push(left.toString());
+            }
+            ms.close();
+            uzs.close();
+            stream.close();
         })
     });
-    console.log(ret);
+    return ret;
 }
-
-search(['124.207.253.85'], ['2017-08-28/04:28'], '117.100.187.110')
+module.exports = {
+    search: search,
+    getUser: getUser,
+    setUser: setUser
+};
